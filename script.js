56f8d592949db51fba374dff96691e204661d7bf
oangelang <98145295+oangelang@users.noreply.github.com>
2023-09-24 08:37:01 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // Array containing your specific text options
 const textArray = [
-    'SEND ITTTTTT',
-    'WOWWWW',
-    'LOL',
-    'Wanker',
+    'send it',
+    'wow',
+    'lol',
+    'wanker',
     'truetruetruetrue',
     'Senioritas?',
     'Angela is the GOAT',
@@ -17,5 +17,34 @@ function getRandomIndex(max) {
     return Math.floor(Math.random() * max);
 }
 
-// Insert random text into HTML
-document.getElementById('randomText').innerHTML = textArray[getRandomIndex(textArray.length)];
+// Function to fetch wind data from OpenWeather API
+async function fetchWindData() {
+    const apiKey = '98a0078877d9f69b70fd98e90c7aa8df'; // Replace with your API Key
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=Doha&appid=${apiKey}`;
+    
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        const windSpeed = data.wind.speed;
+        return windSpeed;
+    } catch (error) {
+        console.error("Error fetching wind data:", error);
+        return null;
+    }
+}
+
+// Main function to display random text and wind data
+async function displayData() {
+    const windSpeed = await fetchWindData();
+    
+    let text = 'Today, Blonde Jesus thinks... ' + textArray[getRandomIndex(textArray.length)];
+
+    if (windSpeed !== null) {
+        text += `<br><br>The wind speed today is ${windSpeed} m/s.`;
+    }
+    
+    document.getElementById('randomText').innerHTML = text;
+}
+
+// Call the function to display data
+displayData();
